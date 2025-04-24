6095538e45a8fff9770a93f70807d32623c23385
Robert Baruck <robert.baruck@sandstorm.de>
2025-04-24 10:56:18 +0200

    TASK: Fix imports

--- Classes/LazyDataSourceTrait.php
+++ Classes/LazyDataSourceTrait.php
@@ -1,17 +1,17 @@
 <?php
 namespace Sandstorm\LazyDataSource;
 
-use Neos\ContentRepository\Domain\Model\NodeInterface;
+use Neos\ContentRepository\Core\Projection\ContentGraph\Node;
 
 trait LazyDataSourceTrait
 {
 
     /**
-     * @param \Neos\ContentRepository\Core\Projection\ContentGraph\Node $node The node that is currently edited (optional)
+     * @param Node|null $node The node that is currently edited (optional)
      * @param array $arguments Additional arguments (key / value)
      * @return array
      */
-    public function getData(\Neos\ContentRepository\Core\Projection\ContentGraph\Node $node = null, array $arguments = [])
+    public function getData(Node $node = null, array $arguments = []): array
     {
         if (isset($arguments['identifiers'])) {
             $identifiers = $arguments['identifiers'];
@@ -31,19 +31,19 @@ trait LazyDataSourceTrait
      * these data records specifically.
      *
      * @param array $identifiers
-     * @param \Neos\ContentRepository\Core\Projection\ContentGraph\Node|null $node
+     * @param Node|null $node
      * @param array $arguments
      * @return mixed
      */
-    abstract protected function getDataForIdentifiers(array $identifiers, \Neos\ContentRepository\Core\Projection\ContentGraph\Node $node = null, array $arguments = []);
+    abstract protected function getDataForIdentifiers(array $identifiers, Node $node = null, array $arguments = []);
 
     /**
      * This method is called when the user specifies a search term.
      *
      * @param string $searchTerm
-     * @param \Neos\ContentRepository\Core\Projection\ContentGraph\Node|null $node
+     * @param Node|null $node
      * @param array $arguments
      * @return mixed
      */
-    abstract protected function searchData(string $searchTerm, \Neos\ContentRepository\Core\Projection\ContentGraph\Node $node = null, array $arguments = []);
+    abstract protected function searchData(string $searchTerm, Node $node = null, array $arguments = []);
 }
